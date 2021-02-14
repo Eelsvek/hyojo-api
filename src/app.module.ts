647b3e68edf6b7186542aeadf91181cce95c9fe3
eelsvek <kev.sunlee@gmail.com>
2021-02-14 11:24:43 -0800

    use ormconfig.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,9 @@ import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { TypeOrmModule} from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { User } from './users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'kevin',
-    password: 'asdfasdf',
-    database: 'hyojo',
-    entities: [User],
-    synchronize: true,
-  }), UsersModule],
+  imports: [TypeOrmModule.forRoot(), UsersModule],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,11 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectRepository(User)
-    private usersRepository: Repository<User>,
-  ) {}
-  
+  private readonly logger = new Logger('UsersService');
+
   async create(createUserDto: CreateUserDto){
     const { email } = createUserDto;
     
@@ -20,7 +15,7 @@ export class UsersService {
     try {
       await user.save();
     } catch (e) {
-      Logger.log('Error', e);
+      this.logger.error('Error', e);
     }
   };
 }

1e3c7930593a36212329b254e6f4a3c583c6a2f4
eelsvek <kev.sunlee@gmail.com>
2021-02-27 16:12:28 -0800

    LocalAuthGuard class

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,10 @@
 import { Controller, Post, UseGuards, Request, Logger } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-
+import { LocalAuthGuard } from "./auth/local-auth.guard";
 @Controller()
 export class AppController {
   private readonly logger = new Logger('AppController');
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     this.logger.log('req', req);
